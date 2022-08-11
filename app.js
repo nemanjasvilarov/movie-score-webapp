5d82334b693f0b1f3b754e2c2f101566660cc0f7
Nemanja Svilarov <nemanja.svilarov@gmail.com>
2022-08-12 01:26:51 +0200

    Adding port

--- app.js
+++ app.js
@@ -112,11 +112,7 @@ app.get('/details/:id', async (req, res) => {
     res.render('moviedetailes', { movie: data });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000...');
 });
