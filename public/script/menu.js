fcac86637410d35ec51e2cd927f6c5d3833e0a92
Nemanja Svilarov <nemanja.svilarov@gmail.com>
2022-08-12 04:30:33 +0200

    Made website responsive

---
+++ public/script/menu.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.menu ul');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
